22c1b4c22f66e7b13ca1d00abda58036e23db424
LARRY MARQUARDT <lmrqrdt@gmail.com>
2018-12-14 14:12:54 -0700

    fixed form changed on check error for post-todo

--- client/src/app/components/post-todo/post-todo.component.ts
+++ client/src/app/components/post-todo/post-todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, AfterViewChecked } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -9,28 +9,36 @@ import { NotifierService } from 'angular-notifier';
   templateUrl: './post-todo.component.html',
   styleUrls: ['./post-todo.component.css']
 })
-export class PostTodoComponent {
+export class PostTodoComponent implements AfterViewChecked {
   text = new FormControl('', [Validators.required, Validators.minLength(6)]);
   private readonly notifier: NotifierService;
 
-  constructor(private http: HttpClient, private router: Router, notifierService: NotifierService) {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    notifierService: NotifierService,
+    private cd: ChangeDetectorRef) {
     this.notifier = notifierService;
   }
 
-    onCreatePost() {
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type':  'application/json',
-          'x-auth': localStorage.getItem('token') || ''
-        })
-      };
-      this.http.post('http://localhost:3000/todos', ({ text: this.text.value }), httpOptions)
-      .subscribe((data: HttpResponse<({text: string})>) => {
-        this.notifier.notify( 'success', 'Your to do has been created and saved!' );
-        this.router.navigate(['/todo-list']);
-     }, (error: any) => {
-        this.notifier.notify( 'error', 'Unable to create to do!' );
-     });
+  onCreatePost() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'x-auth': localStorage.getItem('token') || ''
+      })
+    };
+    this.http.post('http://localhost:3000/todos', ({ text: this.text.value }), httpOptions)
+    .subscribe((data: HttpResponse<({text: string})>) => {
+      this.notifier.notify( 'success', 'Your to do has been created and saved!' );
+      this.router.navigate(['/todo-list']);
+    }, (error: any) => {
+      this.notifier.notify( 'error', 'Unable to create to do!' );
+    });
+  }
+
+  ngAfterViewChecked(): void {
+    this.cd.detectChanges();
   }
 }
 
